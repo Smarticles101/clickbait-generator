82b0bac15b61206894c7ec73e3c963ab541dce8a
Logan Stucki <logan@loganstucki.me>
2020-10-10 22:08:37 -0600

    use env.PORT

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import express from "express";
 import { ClickbaitRouter } from "./routes/clickbait";
+const port = process.env.PORT || 8080;
 
 const app = express();
-const port = 3000;
 app.get("/", (req, res) => {
   res.send("The sedulous hyena ate the antelope!");
 });
