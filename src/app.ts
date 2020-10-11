d6ecf0854b28409812507e7cd0e3026a43c39c88
Logan Stucki <logan@loganstucki.me>
2020-10-10 21:33:11 -0600

    add prettier

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
-import express from 'express';
-import { ClickbaitRouter } from './routes/clickbait';
+import express from "express";
+import { ClickbaitRouter } from "./routes/clickbait";
 
 const app = express();
 const port = 3000;
-app.get('/', (req, res) => {
-  res.send('The sedulous hyena ate the antelope!');
+app.get("/", (req, res) => {
+  res.send("The sedulous hyena ate the antelope!");
 });
 
-app.use('/api/', ClickbaitRouter);
+app.use("/api/", ClickbaitRouter);
 
 app.listen(port, () => {
   return console.log(`server is listening on ${port}`);
